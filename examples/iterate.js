f5856f7ed14235549563e958925ca77fd28bfc05
Martin Schuhfuss <m.schuhfuss@googlemail.com>
2015-05-28 00:53:37 +0200

    add new example that just iterates through all leds

---
+++ examples/iterate.js
@@ -0,0 +1,30 @@
+var ws281x = require('../index.js');
+
+var NUM_LEDS = parseInt(process.argv[2], 10) || 10,
+    pixelData = new Uint32Array(NUM_LEDS);
+
+ws281x.init(NUM_LEDS);
+
+ws281x.setIndexMapping(ws281x.indexMapping.mirrorMatrixX(10,10));
+
+// ---- trap the SIGINT and reset before exit
+process.on('SIGINT', function () {
+  ws281x.reset();
+  process.nextTick(function () { process.exit(0); });
+});
+
+
+// ---- animation-loop
+var offset = 0;
+setInterval(function () {
+  var i=NUM_LEDS;
+  while(i--) {
+      pixelData[i] = 0;
+  }
+  pixelData[offset] = 0xffffff;
+
+  offset = (offset + 1) % NUM_LEDS;
+  ws281x.render(pixelData);
+}, 100);
+
+console.log('Press <ctrl>+C to exit.');
