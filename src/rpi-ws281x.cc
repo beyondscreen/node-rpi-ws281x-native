75593291e2bb21e179898971f9e9c3a08ff65d82
Martin Schuhfuss <m.schuhfuss@googlemail.com>
2015-10-05 00:48:56 +0200

    update to nan-2.0.9
     - now compatible with node-4.x
     - update all nan-functions to new versions
     - replace internal format to Buffer

--- src/rpi-ws281x.cc
+++ src/rpi-ws281x.cc
@@ -1,6 +1,6 @@
-#include <node.h>
-#include <v8.h>
 #include <nan.h>
+
+#include <v8.h>
 
 #include <stdio.h>
 #include <stdint.h>
@@ -28,50 +28,35 @@ ws2811_channel_t
  *   if data is longer than the number of leds, remaining data will be ignored.
  *   Otherwise, data
  */
-NAN_METHOD(Render) {
-  NanScope();
-
-  if(args.Length() != 1) {
-    return NanThrowTypeError("render(): missing argument.");
+void render(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+  if(info.Length() != 1) {
+    Nan::ThrowTypeError("render(): missing argument.");
+    return;
   }
 
-  if(!args[0]->IsUint32Array()) {
-    return NanThrowTypeError(
-        "render(): expected argument to be an Uint32Array.");
+  if(!node::Buffer::HasInstance(info[0])) {
+    Nan::ThrowTypeError("render(): expected argument to be a Buffer.");
+    return;
   }
 
-  Local<Uint32Array> arr = Local<Uint32Array>::Cast(args[0]->ToObject());
+  Local<Object> buffer = info[0]->ToObject();
 
-  if(!arr->HasIndexedPropertiesInExternalArrayData()) {
-    // This is still a bit mysterious why this happens, but arrays with less
-    // than 16 elements are not created with their indexed properties in an
-    // external array. In this case a call to the Buffer-method will trigger
-    // JSTypedArray::MaterializeArrayBuffer which in turn converts the array
-    // into an external array.
-    arr->Buffer();
-  }
+  int numBytes = std::min((int)node::Buffer::Length(buffer),
+      4 * ledstring.channel[0].count);
 
-  if (arr->GetIndexedPropertiesExternalArrayDataType() != kExternalUnsignedIntArray) {
-    return NanThrowTypeError("render(): something did go wrong. Please file a bugreport.");
-  }
-
-  int len = arr->GetIndexedPropertiesExternalArrayDataLength();
-  uint32_t* data = static_cast<uint32_t*>(arr->GetIndexedPropertiesExternalArrayData());
-
-  memcpy(ledstring.channel[0].leds, data, std::min(4*len, 4*ledstring.channel[0].count));
+  uint32_t* data = (uint32_t*) node::Buffer::Data(buffer);
+  memcpy(ledstring.channel[0].leds, data, numBytes);
 
   ws2811_wait(&ledstring);
   ws2811_render(&ledstring);
 
-  NanReturnValue(NanUndefined());
+  info.GetReturnValue().SetUndefined();
 }
 
 /**
  * exports.init(Number ledCount [, Object config]) - setup the configuration and initialize the library.
  */
-NAN_METHOD(Init) {
-  NanScope();
-
+void init(const Nan::FunctionCallbackInfo<v8::Value>& info) {
   ledstring.freq    = DEFAULT_TARGET_FREQ;
   ledstring.dmanum  = DEFAULT_DMANUM;
 
@@ -88,27 +73,27 @@ NAN_METHOD(Init) {
   ledstring.channel[0] = channel0data;
   ledstring.channel[1] = channel1data;
 
-  if(args.Length() < 1) {
-    return NanThrowTypeError("init(): expected at least 1 argument");
+  if(info.Length() < 1) {
+    return Nan::ThrowTypeError("init(): expected at least 1 argument");
   }
 
   // first argument is a number
-  if(!args[0]->IsNumber()) {
-    return NanThrowTypeError("init(): argument 0 is not a number");
+  if(!info[0]->IsNumber()) {
+    return Nan::ThrowTypeError("init(): argument 0 is not a number");
   }
 
-  ledstring.channel[0].count = args[0]->Int32Value();
+  ledstring.channel[0].count = info[0]->Int32Value();
 
   // second (optional) an Object
-  if(args.Length() >= 2 && args[1]->IsObject()) {
-    Local<Object> config = args[1]->ToObject();
+  if(info.Length() >= 2 && info[1]->IsObject()) {
+    Local<Object> config = info[1]->ToObject();
 
     Local<String>
-        symFreq = NanNew<String>("frequency"),
-        symDmaNum = NanNew<String>("dmaNum"),
-        symGpioPin = NanNew<String>("gpioPin"),
-        symInvert = NanNew<String>("invert"),
-        symBrightness = NanNew<String>("brightness");
+        symFreq = Nan::New<String>("frequency").ToLocalChecked(),
+        symDmaNum = Nan::New<String>("dmaNum").ToLocalChecked(),
+        symGpioPin = Nan::New<String>("gpioPin").ToLocalChecked(),
+        symInvert = Nan::New<String>("invert").ToLocalChecked(),
+        symBrightness = Nan::New<String>("brightness").ToLocalChecked();
 
     if(config->HasOwnProperty(symFreq)) {
       ledstring.freq = config->Get(symFreq)->Uint32Value();
@@ -135,33 +120,31 @@ NAN_METHOD(Init) {
   int err = ws2811_init(&ledstring);
 
   if(err) {
-      return NanThrowError("init(): initialization failed. sorry – no idea why.");
+      return Nan::ThrowError("init(): initialization failed. sorry – no idea why.");
   }
 
-  NanReturnValue(NanUndefined());
+  info.GetReturnValue().SetUndefined();
 }
 
 /**
  * exports.reset() – blacks out the LED-strip and finalizes the library
  * (disable PWM, free DMA-pages etc).
  */
-NAN_METHOD(Reset) {
-  NanScope();
-
+void reset(const Nan::FunctionCallbackInfo<v8::Value>& info) {
   memset(ledstring.channel[0].leds, 0, sizeof(*ledstring.channel[0].leds) * ledstring.channel[0].count);
 
   ws2811_render(&ledstring);
   ws2811_wait(&ledstring);
   ws2811_fini(&ledstring);
 
-  NanReturnValue(NanUndefined());
+  info.GetReturnValue().SetUndefined();
 }
 
 
 void initialize(Handle<Object> exports) {
-  exports->Set(NanNew<String>("init"),   NanNew<FunctionTemplate>(Init)->GetFunction());
-  exports->Set(NanNew<String>("reset"),  NanNew<FunctionTemplate>(Reset)->GetFunction());
-  exports->Set(NanNew<String>("render"), NanNew<FunctionTemplate>(Render)->GetFunction());
+  NAN_EXPORT(exports, init);
+  NAN_EXPORT(exports, reset);
+  NAN_EXPORT(exports, render);
 }
 
 NODE_MODULE(rpi_ws281x, initialize)
