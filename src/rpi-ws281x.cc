6785a8e85196934724194cf78f2a7a3d0e089615
Martin Schuhfuss <m.schuhfuss@gmail.com>
2021-03-23 14:32:45 +0100

    fix: updatess: nan, rpi_ws281x, remove rpi-version detection

--- src/rpi-ws281x.cc
+++ src/rpi-ws281x.cc
@@ -1,10 +1,8 @@
 #include <nan.h>
-
-#include <v8.h>
-
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <string.h>
+#include <v8.h>
 
 #include <algorithm>
 
@@ -32,41 +30,37 @@ ws2811_t ws281x;
  * ws281x.setParam(param:Number, value:Number)
  * wrap setting global params in ws2811_t
  */
-void setParam(const Nan::FunctionCallbackInfo<v8::Value> &info)
-{
-  if (info.Length() != 2)
-  {
+void setParam(const Nan::FunctionCallbackInfo<v8::Value> &info) {
+  if (info.Length() != 2) {
     Nan::ThrowTypeError("setParam(): expected two params");
     return;
   }
 
-  if (!info[0]->IsNumber())
-  {
-    Nan::ThrowTypeError("setParam(): expected argument 1 to be the parameter-id");
+  if (!info[0]->IsNumber()) {
+    Nan::ThrowTypeError(
+        "setParam(): expected argument 1 to be the parameter-id");
     return;
   }
 
-  if (!info[1]->IsNumber())
-  {
+  if (!info[1]->IsNumber()) {
     Nan::ThrowTypeError("setParam(): expected argument 2 to be the value");
     return;
   }
 
-  const int param = info[0]->Int32Value();
-  const int value = info[1]->Int32Value();
+  const int param = Nan::To<int32_t>(info[0]).FromJust();
+  const int value = Nan::To<int32_t>(info[1]).FromJust();
 
-  switch (param)
-  {
-  case PARAM_FREQ:
-    ws281x.freq = value;
-    break;
-  case PARAM_DMANUM:
-    ws281x.dmanum = value;
-    break;
+  switch (param) {
+    case PARAM_FREQ:
+      ws281x.freq = value;
+      break;
+    case PARAM_DMANUM:
+      ws281x.dmanum = value;
+      break;
 
-  default:
-    Nan::ThrowTypeError("setParam(): invalid parameter-id");
-    return;
+    default:
+      Nan::ThrowTypeError("setParam(): invalid parameter-id");
+      return;
   }
 }
 /**
@@ -74,65 +68,61 @@ void setParam(const Nan::FunctionCallbackInfo<v8::Value> &info)
  *
  * wrap setting params in ws2811_channel_t
  */
-void setChannelParam(const Nan::FunctionCallbackInfo<v8::Value> &info)
-{
-  if (info.Length() != 3)
-  {
+void setChannelParam(const Nan::FunctionCallbackInfo<v8::Value> &info) {
+  if (info.Length() != 3) {
     Nan::ThrowTypeError("setChannelParam(): missing argument");
     return;
   }
 
   // retrieve channelNumber from argument 1
-  if (!info[0]->IsNumber())
-  {
-    Nan::ThrowTypeError("setChannelParam(): expected argument 1 to be the channel-number");
+  if (!info[0]->IsNumber()) {
+    Nan::ThrowTypeError(
+        "setChannelParam(): expected argument 1 to be the channel-number");
     return;
   }
 
-  const int channelNumber = info[0]->Int32Value();
-  if (channelNumber > 1 || channelNumber < 0)
-  {
+  const int channelNumber = Nan::To<int32_t>(info[0]).FromJust();
+  if (channelNumber > 1 || channelNumber < 0) {
     Nan::ThrowError("setChannelParam(): invalid chanel-number");
     return;
   }
 
-  if (!info[1]->IsNumber())
-  {
-    Nan::ThrowTypeError("setChannelParam(): expected argument 2 to be the parameter-id");
+  if (!info[1]->IsNumber()) {
+    Nan::ThrowTypeError(
+        "setChannelParam(): expected argument 2 to be the parameter-id");
     return;
   }
 
-  if (!info[2]->IsNumber() && !info[2]->IsBoolean())
-  {
-    Nan::ThrowTypeError("setChannelParam(): expected argument 3 to be the value");
+  if (!info[2]->IsNumber() && !info[2]->IsBoolean()) {
+    Nan::ThrowTypeError(
+        "setChannelParam(): expected argument 3 to be the value");
     return;
   }
 
-  ws2811_channel_t* channel = &ws281x.channel[channelNumber];
-  const int param = info[1]->Int32Value();
-  const int value = info[2]->Int32Value();
+  ws2811_channel_t *channel = &ws281x.channel[channelNumber];
+  const int param = Nan::To<int32_t>(info[1]).FromJust();
+  const int value = Nan::To<int32_t>(info[2]).FromJust();
 
-  switch (param)
-  {
-  case PARAM_GPIONUM:
-    channel->gpionum = value;
-    break;
-  case PARAM_COUNT:
-    channel->count = value;
-    break;
-  case PARAM_INVERT:
-    channel->invert = value;
-    break;
-  case PARAM_BRIGHTNESS:
-    channel->brightness = (uint8_t)value;
-    break;
-  case PARAM_STRIP_TYPE:
-    channel->strip_type = value;
-    break;
+  switch (param) {
+    case PARAM_GPIONUM:
+      channel->gpionum = value;
+      break;
+    case PARAM_COUNT:
+      channel->count = value;
+      break;
+    case PARAM_INVERT:
+      channel->invert = value;
+      break;
+    case PARAM_BRIGHTNESS:
+      channel->brightness = (uint8_t)value;
+      break;
+    case PARAM_STRIP_TYPE:
+      channel->strip_type = value;
+      break;
 
-  default:
-    Nan::ThrowTypeError("setChannelParam(): invalid parameter-id");
-    return;
+    default:
+      Nan::ThrowTypeError("setChannelParam(): invalid parameter-id");
+      return;
   }
 }
 
@@ -141,47 +131,42 @@ void setChannelParam(const Nan::FunctionCallbackInfo<v8::Value> &info)
  *
  * wrap copying data to ws2811_channel_t.leds
  */
-void setChannelData(const Nan::FunctionCallbackInfo<v8::Value> &info)
-{
-  if (info.Length() != 2)
-  {
+void setChannelData(const Nan::FunctionCallbackInfo<v8::Value> &info) {
+  if (info.Length() != 2) {
     Nan::ThrowTypeError("setChannelData(): missing argument.");
     return;
   }
 
   // retrieve channelNumber from argument 1
-  if (!info[0]->IsNumber())
-  {
-    Nan::ThrowTypeError("setChannelData(): expected argument 1 to be the channel-number.");
+  if (!info[0]->IsNumber()) {
+    Nan::ThrowTypeError(
+        "setChannelData(): expected argument 1 to be the channel-number.");
     return;
   }
 
-  int channelNumber = info[0]->Int32Value();
-  if (channelNumber > 1 || channelNumber < 0)
-  {
+  int channelNumber = Nan::To<int32_t>(info[0]).FromJust();
+  if (channelNumber > 1 || channelNumber < 0) {
     Nan::ThrowError("setChannelData(): invalid chanel-number");
     return;
   }
   ws2811_channel_t channel = ws281x.channel[channelNumber];
 
   // retrieve buffer from argument 2
-  if (!node::Buffer::HasInstance(info[1]))
-  {
+  if (!node::Buffer::HasInstance(info[1])) {
     Nan::ThrowTypeError("setChannelData(): expected argument 2 to be a Buffer");
     return;
   }
-  Local<Object> buffer = info[1]->ToObject();
+  v8::Local<v8::Context> context = info.GetIsolate()->GetCurrentContext();
+  auto buffer = info[1]->ToObject(context).ToLocalChecked();
   uint32_t *data = (uint32_t *)node::Buffer::Data(buffer);
 
-  if (channel.count == 0 || channel.leds == NULL)
-  {
+  if (channel.count == 0 || channel.leds == NULL) {
     Nan::ThrowError("setChannelData(): channel not ready");
     return;
   }
 
-  const int numBytes = std::min(
-      node::Buffer::Length(buffer),
-      sizeof(ws2811_led_t) * ws281x.channel[0].count);
+  const int numBytes = std::min(node::Buffer::Length(buffer),
+                                sizeof(ws2811_led_t) * ws281x.channel[0].count);
 
   // FIXME: handle memcpy-result
   memcpy(channel.leds, data, numBytes);
@@ -192,13 +177,11 @@ void setChannelData(const Nan::FunctionCallbackInfo<v8::Value> &info)
  *
  * wrap ws2811_init()
  */
-void init(const Nan::FunctionCallbackInfo<v8::Value> &info)
-{
+void init(const Nan::FunctionCallbackInfo<v8::Value> &info) {
   ws2811_return_t ret;
 
   ret = ws2811_init(&ws281x);
-  if (ret != WS2811_SUCCESS)
-  {
+  if (ret != WS2811_SUCCESS) {
     Nan::ThrowError(ws2811_get_return_t_str(ret));
     return;
   }
@@ -209,20 +192,17 @@ void init(const Nan::FunctionCallbackInfo<v8::Value> &info)
  *
  * wrap ws2811_wait() and ws2811_render()
  */
-void render(const Nan::FunctionCallbackInfo<v8::Value> &info)
-{
+void render(const Nan::FunctionCallbackInfo<v8::Value> &info) {
   ws2811_return_t ret;
 
   ret = ws2811_wait(&ws281x);
-  if (ret != WS2811_SUCCESS)
-  {
+  if (ret != WS2811_SUCCESS) {
     Nan::ThrowError(ws2811_get_return_t_str(ret));
     return;
   }
 
   ret = ws2811_render(&ws281x);
-  if (ret != WS2811_SUCCESS)
-  {
+  if (ret != WS2811_SUCCESS) {
     Nan::ThrowError(ws2811_get_return_t_str(ret));
     return;
   }
@@ -233,13 +213,11 @@ void render(const Nan::FunctionCallbackInfo<v8::Value> &info)
  *
  * wrap ws2811_wait() and ws2811_fini()
  */
-void finalize(const Nan::FunctionCallbackInfo<v8::Value> &info)
-{
+void finalize(const Nan::FunctionCallbackInfo<v8::Value> &info) {
   ws2811_return_t ret;
 
   ret = ws2811_wait(&ws281x);
-  if (ret != WS2811_SUCCESS)
-  {
+  if (ret != WS2811_SUCCESS) {
     Nan::ThrowError(ws2811_get_return_t_str(ret));
     return;
   }
@@ -250,8 +228,7 @@ void finalize(const Nan::FunctionCallbackInfo<v8::Value> &info)
 /**
  * initializes the module.
  */
-void initialize(Local<Object> exports)
-{
+void initialize(Local<Object> exports) {
   ws281x.freq = DEFAULT_TARGET_FREQ;
   ws281x.dmanum = DEFAULT_DMANUM;
 
